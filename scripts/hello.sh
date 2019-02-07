91948f860f2e3f4beea4ff387f0ad2c3a822783b
Jesstern Rays <jsstrn@users.noreply.github.com>
2019-02-07 17:53:03 +0800

    Add hello script

---
+++ scripts/hello.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# The first line is called a shebang and the absolute path to your preferred interpreter
+
+# What you type on the Terminal can also by typed here and executed. Try executing this script
+echo "Hello, world!"
+
+# You cannot execute this file because you don't have permission to do so
+# Change permissions with `chmod +x hello.sh` and verify with `ls -l`
+# Now try and execute this file again
